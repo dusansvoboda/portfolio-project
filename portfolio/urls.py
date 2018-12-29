700b00a2285116567087f7d55c346c614a0a3e21
Dusan <dusan.svoboda@gmail.com>
2018-12-29 17:35:40 +0000

    Jobs up and running

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings # added this line
+from django.conf.urls.static import static # added this line
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # added this line
